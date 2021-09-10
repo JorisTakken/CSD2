8721fa582b601a992dae6efcc6c86a513612dddf
JorisTakken <joristakken@gmail.com>
2021-09-10 17:57:22 +0200

    Audio afspelen doet het

--- CSD2a/Python/playsound.py
+++ CSD2a/Python/playsound.py
@@ -1,9 +1,13 @@
 # import simpleaudio.functionchecks as fc
 
 # fc.LeftRightCheck.run()
+
+
+
 import simpleaudio as sa
 
 filename = 'schreeuw.wav'
 wave_obj = sa.WaveObject.from_wave_file(filename)
 play_obj = wave_obj.play()
 play_obj.wait_done()
+
