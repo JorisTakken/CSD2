3894cd6b4dae88762333df957e60963c1146165a
JorisTakken <joristakken@gmail.com>
2021-09-10 13:32:59 +0200

    eerste gesprek met python

--- CSD2a/Python/hoi.py
+++ CSD2a/Python/hoi.py
@@ -1,2 +1,8 @@
-username = input ("Joris schatje")
-print("ik ben",username)
+
+print('hallo!')
+hallo = input("")
+print('welkom, wat is je naam?')
+mijnNaam = input("")
+print("leuk je te ontmoeten", mijnNaam + ",", "hoe oud ben je?")
+leeftijd = input("")
+print("jezus man!", leeftijd, "?!?! ouwe tak!")
