47d9956c84826b2c4327b89333c8e873055db36f
JorisTakken <joristakken@gmail.com>
2021-09-10 16:37:05 +0200

    Ja

--- CSD2a/Python/playsound.py
+++ CSD2a/Python/playsound.py
@@ -3,4 +3,7 @@
 # fc.LeftRightCheck.run()
 import simpleaudio as sa
 
-
+filename = 'schreeuw.wav'
+wave_obj = sa.WaveObject.from_wave_file(filename)
+play_obj = wave_obj.play()
+play_obj.wait_done()
