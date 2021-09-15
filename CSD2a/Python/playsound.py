b1b28727082029a11ed632bd3d9abed50a8dbdb9
Joris Takken <joristakken@gmail.com>
2021-09-15 11:03:45 +0200

    random

--- CSD2a/Python/playsound.py
+++ CSD2a/Python/playsound.py
@@ -6,10 +6,6 @@ import simpleaudio as sa
 
 filename = 'schreeuw.wav'
 wave_obj = sa.WaveObject.from_wave_file(filename)
-
-print("wil jij geluid horen?")
-input = ("")
-print("oke komt ie!")
 play_obj = wave_obj.play()
 play_obj.wait_done()
 
