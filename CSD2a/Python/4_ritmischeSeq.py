34cb212f7621084811f8202f0ea9a488987e3aeb
Joris Takken <joristakken@gmail.com>
2021-09-28 12:55:51 +0200

    laatste versie voor de les

--- CSD2a/Python/4_ritmischeSeq.py
+++ CSD2a/Python/4_ritmischeSeq.py
@@ -2,8 +2,6 @@
 import time
 import simpleaudio as sa
 
-
-
 # importeer samples en geef de samples namen
 kick = sa.WaveObject.from_wave_file("samples/kick.wav")
 
@@ -11,84 +9,70 @@ kick = sa.WaveObject.from_wave_file("samples/kick.wav")
 print("--------------------------------------")
 print("hoeveel keer wil je de sample spelen?")
 print("--------------------------------------")
-hoeveelKeerSpelen = float(input())
-
+hoeveelKeerSpelen = int(input())
 
 # kies een BPM
 print("---------------------")
 print("wat voor BPM kies je?")
 print("---------------------")
 BPMinput = float(input())
-BPM = 60 / BPMinput # in ms
+BPM = 60.0 / BPMinput # in ms
 
 # dit is de input voor het kiezen tussen een random nootduur of nootduur die je zelf kiest
-
 print("------------------------------")
 print("Kies uit:")
 print("kwart (4)| half (2) | heel (1)")
 print("------------------------------")
-
-
 duratieNoot = []
-i = 1
-while i < (1 + hoeveelKeerSpelen):
+for i in range(hoeveelKeerSpelen):
     print ("duratie", i)
-    keuze = float(input(""))
+    keuze = (float(input("")))
     if(keuze == 4):
         duratieNoot.append(0.25)
     if(keuze == 2):
         duratieNoot.append(0.5)
     if(keuze == 1):
         duratieNoot.append(1)
-    i += 1
 
 
+
+# je begint bij 0 en elke value heeft een eigen waarde bij de array
 tijd16eNoten = []
 laatsteVal = 0
-for i in duratieNoot:
+for i in (duratieNoot):
     tijd16eNoten.append(laatsteVal)
     if i == 0.25:
         laatsteVal += 1
-    elif i == 0.50:
+    elif i == 0.50: 
         laatsteVal += 2
     elif i == 1:
         laatsteVal += 4
 
-
-
+# hier convert je de 16e stappen weer naar tijds intervallen
 tijdStempels = []
-tijdVan1Zestiende = BPM/4
+tijdVan1Zestiende = BPM
 for i in tijd16eNoten:
     tijdStempels.append(float(i * tijdVan1Zestiende))
 
-
 tijdBegin = time.time()
 
+print(tijdStempels)
 
 if tijdStempels:
     ts = tijdStempels.pop(0)
 
-
-while True:
-    nu = float(time.time() - tijdBegin)  
-    for i in tijdStempels:
+while True:       
+    now = time.time() - tijdBegin
+    if (now >= ts):
         print(ts)
-        if (nu >= ts):
-            kick.play()
-            if tijdStempels:
-                ts = tijdStempels.pop(0)
+        kick.play()
+        if tijdStempels:
+            ts = tijdStempels.pop(0)
+            
     time.sleep(0.001)
-    
+time.sleep(tijd16eNoten[-1])
+
+
+
 
   
-   
-   
-
-    
-
-
-    
-
-
-        
-
