27eb6590523a2db55a38539c309f57a42dc1d371
Joris Takken <joristakken@gmail.com>
2021-10-12 14:59:37 +0200

    even sorteren en orde scheppen

--- CSD2a/Python/6_minderRand.py
+++ CSD2a/Python/6_minderRand.py
@@ -1,7 +1,20 @@
+#IDEE
+
+# de Kick heeft 2 plekken
+# Kick      1  |  0           
+#           0  |  1       
+#           1  |  1
+
+# snare heeft 3 plekken over de tijd van de 2 plekken van de Kick
+# snare     50% | 25% | 50%
+
+# Hat heeft 5 plekken over de tijd vand de 2 pekken van de kick
+# hat       1 | 50% | 75% | 50% | 1
+
 import simpleaudio as sa
 import time
 import random
-
+import wave
 
 hat = sa.WaveObject.from_wave_file("samples_drumstel/hat.wav") 
 snare = sa.WaveObject.from_wave_file("samples_drumstel/snare.wav")
@@ -9,7 +22,12 @@ kick = sa.WaveObject.from_wave_file("samples_drumstel/kick.wav")
 bongo1 = sa.WaveObject.from_wave_file("samples_drumstel/bongo1.wav")
 bongo2 = sa.WaveObject.from_wave_file("samples_drumstel/bongo2.wav")
 
-BPMinput = 120
+# kies een BPM
+print("---------------------")
+print("wat voor BPM kies je?")
+print("---------------------")
+BPMinput = float(input())
+
 BPMHeel = (60.0 / BPMinput) * 2  #= 1 hele noot  
 BPMTwee = BPMHeel / 2            #= 1/2 noot  
 BPMDrie = BPMHeel / 3            #= 1/3 noot  
@@ -28,57 +46,88 @@ def event_stamps(welkBlok,plek):
         "plek" : plek,   
     }
 
-# arrays voor welke plekken er beschikbaar zijn in de groepen
-plekTweetje = [1,2]
-plekDrietje = [1,2,3]
-plekVijftje = [1,2,3,4,5]
-plekAchtje = [1,2,3,4,5,6,7,8]
-
 # arrays voor de timestamps van de groepen 
 stempels = []
 
 def randomD():
-    for i in plekTweetje:
-        rand = random.randint(1,100)
+# tweetje (kick)
+# ------------------------------------------------
+    # for i in plekTweetje:
+    rand = random.randint(1,100)
+    rand1 = random.randint(1,100)
         # dus hij zal vaker op de 1 een beat geven dan op de 2
-        if (rand <= 50):
-            stempels.append(event_stamps("twee",i))
+        # en er is 50% kans dat ie na de 1 ook op de 2 speelt
+    if (rand <= 80):
+        stempels.append(event_stamps("twee",1))
+        if (rand1 <= 50):
+            stempels.append(event_stamps("twee",2))
+    else:
+        stempels.append(event_stamps("twee",2))
+# ------------------------------------------------
 
-    for i in plekDrietje:
-        rand10 = random.randint(1,100)
-        rand11 = random.randint(1,100)
-        rand12 = random.randint(1,100)
-        # dus hij zal vaker op de 1 een beat geven dan op de 2
-        if (rand10 <= 50):
-            stempels.append(event_stamps("drie",plekDrietje[0]))
-            print('1')
-        if (rand11 <= 25):
-            stempels.append(event_stamps("drie",plekDrietje[1]))
-            print('2')
-        if (rand12 <= 50):
-            stempels.append(event_stamps("drie",plekDrietje[2]))
-            print('3')
-        
-        
-    for i in plekVijftje:
-        rand2 = random.randint(1,100)
-        # dus hij zal vaker op de 1 een beat geven dan op de 2
-        if (rand2 <= 80):
-            stempels.append(event_stamps("vijf",i))
+# Drietje (snare)
+# ------------------------------------------------
 
+    rand10 = random.randint(1,100)
+    rand11 = random.randint(1,100)
+    rand12 = random.randint(1,100)
+    # snare op 1 = 50 % kans
+    # snare op 2 = 25 % kans
+    # snare op 3 = 50 % kans
+    if (rand10 <= 50):
+        stempels.append(event_stamps("drie",1))
+
+    if (rand11 <= 25):
+        stempels.append(event_stamps("drie",2))
+
+    if (rand12 <= 50):
+        stempels.append(event_stamps("drie",3))
+        
+# ------------------------------------------------
+
+# vijfje (hat)
+# ------------------------------------------------
+    rand20 = random.randint(1,100)
+    rand21 = random.randint(1,100)
+    rand22 = random.randint(1,100)
+    rand23 = random.randint(1,100)
+    rand24 = random.randint(1,100)
+
+    # hat       70% | 50% | 75% | 50% | 1
+    if (rand20 <= 70):
+        stempels.append(event_stamps("vijf",1))
+    if (rand21 <= 50):
+        stempels.append(event_stamps("vijf",2))
+    if (rand22 <= 75):
+        stempels.append(event_stamps("vijf",3))
+    if (rand23 <= 50):
+        stempels.append(event_stamps("vijf",4))
+    if (rand24 <= 100):
+        stempels.append(event_stamps("vijf",5))
+# ------------------------------------------------
+
+# achtje (bongo)
+# ------------------------------------------------
+# array voor bongo, voor welke plekken er beschikbaar zijn 
+    plekAchtje = [1,2,3,4,5,6,7,8]
     for i in plekAchtje:
         rand3 = random.randint(1,100)
-        # dus hij zal vaker op de 1 een beat geven dan op de 2
+
         if (rand3 <= 80):
             stempels.append(event_stamps("acht",i))
+# ------------------------------------------------
+
 
 # de timestamps van alle instrumenten / lengtesworden in 1 lijst gestopt samen met de instrument Naam
 alleStamps = []
 
+randomD()
+print(stempels)
+
+
 # hierin worden de plekken van de timestamps geconverteerd naar MS
 def bijElkaar():
     randomD()
-    
     samples = ['kick','snare','hat','bongo1','bongo2']
     for i in stempels:
         rand4 = random.randint(1,100)
@@ -94,11 +143,9 @@ def bijElkaar():
                 alleStamps.append(event_instrument(samples[3],i['plek']*BPMAcht))
             else:
                 alleStamps.append(event_instrument(samples[4],i['plek']*BPMAcht))
-        stempels.clear()
-        
 
     # als ie alle timestamps heeft gemaakt mag de lijst gecleard worden voor een nieuwe reeks
-    
+    stempels.clear()
 
 # roep voor de eerste keer de functie aan
 bijElkaar()
@@ -114,15 +161,22 @@ while True:
         if (nu >= i['timestamps']): 
             if i["instrument"] == 'kick':
                 kick.play()
+                print("                                                     ʕ•ᴥ•ʔ")
             if i["instrument"] == 'snare':
                 snare.play()
+                print("                         (▀̿Ĺ̯▀̿ ̿)")
             if i["instrument"] == 'hat':
                 hat.play()
+                print("(͡• ͜ʖ ͡•)")
+
 
             if i["instrument"] == 'bongo1':
                 bongo1.play()
+                print("         (；☉_☉)")
             if i["instrument"] == 'bongo2':
                 bongo2.play()
+                print("                                     (☉_☉ ；)")
+
                 
             # elke keer als er een geluid is gespeeld moet er 1 geluid weg
             alleStamps.remove(i)
@@ -130,11 +184,16 @@ while True:
             # elke keer als de lijst leeg is zal er een nieuwe begin tijd komen
             # de lijst zal ook elke keer weer gevult worden
             if alleStamps == []:
-                print("-=-=-=-=-=-=-=-=-=-=-=-=-")
+                print(" ")
+                print("༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽   ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽")
+                print(" ")
                 tijdBegin = time.time()
                 nu = time.time() - tijdBegin
                 bijElkaar()
-                
+
+
+
+
                 
                 
                 
--- CSD2a/Python/kladbolk_andereaudioding.py
+++ CSD2a/Python/kladbolk_andereaudioding.py
@@ -1,28 +1,16 @@
 from pydub import AudioSegment
 from pydub.playback import play
-from pydub.utils import make_chunks
+import time
 
-# Load into PyDub
-loop = AudioSegment.from_wav("samples_drumstel/snare.wav")
+song = AudioSegment.from_wav("samples_drumstel/kick.wav")
 
-# Repeat 2 times
-loop2 = loop
+# boost volume by 6dB
+louder_song = song + 6
 
-# Get length in milliseconds
-length = len(loop2)
-luidheid = loop.dBFS
+# reduce volume by 3dB
+quieter_song = song - 3
 
-# # Set fade time
-# fade_time = int(length * 0.5)
-
-# # Fade in and out
-# faded = loop2.fade_in(fade_time).fade_out(fade_time)
-
-# # Play the faded loop
-print(length)
-print(luidheid)
-
-play(loop2)
-
-
-
+#Play song
+play(louder_song)
+time.sleep(1)
+play(quieter_song)
