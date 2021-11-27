a9de2356d7031f0e2c80e2495d65fd84c0e6b0ee
Joris Takken <joristakken@gmail.com>
2021-11-27 13:25:05 +0100

    build functions

---
+++ CSD2b/assignments/instrument/build.sh
@@ -0,0 +1,4 @@
+c++ -c instrument.cpp
+c++ -c strings.cpp
+c++ -c main.cpp
+c++ -o instrumenten instrument.o strings.o main.o 
---
+++ CSD2b/assignments/instrument/gitBuild.sh
@@ -0,0 +1,10 @@
+git add instrument.h
+git add instrument.cpp
+git add main.cpp
+git add strings.cpp
+git add strings.h
+git add wind_instrument.cpp
+git add wind_instrument.h
+
+git commit
+
