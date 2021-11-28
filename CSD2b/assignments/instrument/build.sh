2d8c461e3ed4919a11580fa7ef393b745a85f6ab
Joris Takken <joristakken@gmail.com>
2021-11-28 16:44:23 +0100

    update build function

--- CSD2b/assignments/instrument/build.sh
+++ CSD2b/assignments/instrument/build.sh
@@ -1,4 +1,6 @@
 c++ -c instrument.cpp
 c++ -c strings.cpp
+c++ -c wind_instrument.cpp
 c++ -c main.cpp
-c++ -o instrumenten instrument.o strings.o main.o 
+
+c++ -o instrumenten instrument.o wind_instrument.o strings.o main.o 
