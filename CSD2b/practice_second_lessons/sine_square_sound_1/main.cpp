14e422d78c69e26bbfa3eb39d87b9b77d03539cf
Joris Takken <joristakken@gmail.com>
2021-12-06 11:10:24 +0100

    small userinput atleast works

--- CSD2b/practice_second_lessons/sine_square_sound_1/main.cpp
+++ CSD2b/practice_second_lessons/sine_square_sound_1/main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 #include <thread>
 #include "sine.h"
-#include "oscillator.h"
 #include "square.h"
 #include "jack_module.h"
 
 
 int main(int argc,char **argv)
 {
-  // create a JackModule instance
   JackModule jack;
-
-  // init the jack, use program name as JACK client name
   jack.init(argv[0]);
   double samplerate = jack.getSamplerate();
 
-  Sine sine("sine",220, 0.5, samplerate);
-
-  float amplitude = 0.15;
-  //assign a function to the JackModule::onProces
-  jack.onProcess = [&sine, &amplitude](jack_default_audio_sample_t *inBuf,
+  std::string waveform_choose;
+    std::cout << "witch waveform would you like to hear,\n";
+    std::cin >> waveform_choose;
+    std::cout << "__-_,\n";
+  if (waveform_choose == "sine"){
+    Sine sine("sine" ,220, 0.5, samplerate);
+    float amplitude = 0.15;
+    jack.onProcess = [&sine, &amplitude](jack_default_audio_sample_t *inBuf,
     jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
 
     for(unsigned int i = 0; i < nframes; i++) {
@@ -29,10 +28,39 @@ int main(int argc,char **argv)
     amplitude = 0.5;
     return 0;
   };
+  }
+
+
+
+
+  
+  else if (waveform_choose == "square"){ 
+    Square square("sine" ,220, 0.5, samplerate);
+    float amplitude = 0.15;
+    jack.onProcess = [&square, &amplitude](jack_default_audio_sample_t *inBuf,
+    jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
+
+    for(unsigned int i = 0; i < nframes; i++) {
+      outBuf[i] = square.getSample() * amplitude;
+      square.tick();
+    }
+    amplitude = 0.5;
+    return 0;
+  };
+  }
+
+  
+
+
+
+
+
+
+
+
+
 
   jack.autoConnect();
-
-  //keep the program running and listen for user input, q = quit
   std::cout << "\n\nPress 'q' when you want to quit the program.\n";
   bool running = true;
   while (running)
@@ -45,8 +73,6 @@ int main(int argc,char **argv)
         break;
     }
   }
-
-  //end the program
   return 0;
-} // main()
+} 
 
--- CSD2b/practice_second_lessons/sine_square_sound_1/oscillator.cpp
+++ CSD2b/practice_second_lessons/sine_square_sound_1/oscillator.cpp
@@ -14,43 +14,53 @@ Oscillator::~Oscillator(){
 
 
 void Oscillator::playWaveform(){
-    
+
+  // here should be the jack module i think
 
 
-  JackModule jack;
-    jack.onProcess = [&sine, &amplitude](jack_default_audio_sample_t *inBuf,
-    jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
 
-    for(unsigned int i = 0; i < nframes; i++) {
-      outBuf[i] = sine.getSample() * amplitude;
-      sine.tick();
-    }
-    amplitude = 0.5;
-    return 0;
-  };
+      // JackModule jack;
+      //   jack.onProcess = [&sine, &amplitude](jack_default_audio_sample_t *inBuf,
+      //   jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
 
-  jack.autoConnect();
+      //   for(unsigned int i = 0; i < nframes; i++) {
+      //     outBuf[i] = sine.getSample() * amplitude;
+      //     sine.tick();
+      //   }
+      //   amplitude = 0.5;
+      //   return 0;
+      // };
+
+      // jack.autoConnect();
 }
 
 void Oscillator::write_waveFile(std::string waveform){
-    // if (waveform == "sine") {
-    //     Sine sine_make(frequency,amplitude,samplerate);
 
-    //     WriteToFile waveform_file("square_values.csv", true);
-    //     for(int i = 0; i < samplerate; i++) {
-    //         waveform_file.write(std::to_string(sine_make.getSample()) + "\n");
-    //         sine_make.tick();
-    //     }
-    // }
-    // else if (waveform == "square"){
-    //     Square square_make(frequency,amplitude,samplerate);
+  // here you can write the file to python plotter
 
-    //     WriteToFile waveform_file("square_values.csv", true);
-    //     for(int i = 0; i < samplerate; i++) {
-    //         waveform_file.write(std::to_string(square_make.getSample()) + "\n");
-    //         square_make.tick();
-    //     }
-    // }
+
+
+
+
+
+        // if (waveform == "sine") {
+        //     Sine sine_make(frequency,amplitude,samplerate);
+
+        //     WriteToFile waveform_file("square_values.csv", true);
+        //     for(int i = 0; i < samplerate; i++) {
+        //         waveform_file.write(std::to_string(sine_make.getSample()) + "\n");
+        //         sine_make.tick();
+        //     }
+        // }
+        // else if (waveform == "square"){
+        //     Square square_make(frequency,amplitude,samplerate);
+
+        //     WriteToFile waveform_file("square_values.csv", true);
+        //     for(int i = 0; i < samplerate; i++) {
+        //         waveform_file.write(std::to_string(square_make.getSample()) + "\n");
+        //         square_make.tick();
+        //     }
+        // }
 
     
 }
